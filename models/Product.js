6942dbd60c925b81a0e7e00b795462d6fe73ab4e
hhsjaar <hhsjaar@gmail.com>
2025-07-10 21:32:34 +0700

    RevisiOrder

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
+// Cek apakah model sudah ada agar tidak dideklarasikan ulang
+const ProductSchema = new mongoose.Schema({
     userId: { type: String, required: true, ref: "user" },
     name: { type: String, required: true },
     description: { type: String, required: true },
@@ -9,7 +10,7 @@ const productSchema = new mongoose.Schema({
     image: { type: Array, required: true },
     category: { type: String, required: true },
     date: { type: Number, required: true },
-    
+
     // INFORMASI KANTIN
     kantin: { 
         type: String, 
@@ -24,28 +25,27 @@ const productSchema = new mongoose.Schema({
             "Kantin TN 3"
         ]
     },
-    
+
     // INFORMASI NILAI GIZI
-    portionSize: { type: String, required: true }, // Ukuran porsi
-    calories: { type: Number, required: true }, // Kalori
-    totalFat: { type: Number, required: true }, // Lemak total (dalam gram)
-    cholesterol: { type: Number, required: true }, // Kolesterol (dalam mg)
-    sodium: { type: Number, required: true }, // Natrium (Garam) (dalam mg)
-    totalCarbohydrates: { type: Number, required: true }, // Karbohidrat total (dalam gram)
-    protein: { type: Number, required: true }, // Protein (dalam gram)
-    vitaminD: { type: Number, required: true }, // Vitamin D (dalam IU atau mg)
-    calcium: { type: Number, required: true }, // Kalsium (dalam mg)
-    iron: { type: Number, required: true }, // Zat Besi (dalam mg)
-    potassium: { type: Number, required: true }, // Kalium (dalam mg)
-    vitaminA: { type: Number, required: true }, // Vitamin A (dalam IU atau mcg)
-    vitaminC: { type: Number, required: true }, // Vitamin C (dalam mg)
-    
-    // Karbon Jejak
-    karbonMakanan: { type: Number, required: true }, // Jejak karbon untuk makanan (kg CO₂e)
-    karbonPengolahan: { type: Number, required: true }, // Jejak karbon untuk pengolahan (kg CO₂e)
-    karbonTransportasiLimbah: { type: Number, required: true }, // Jejak karbon untuk transportasi dan limbah (kg CO₂e)
+    portionSize: { type: String, required: true },
+    calories: { type: Number, required: true },
+    totalFat: { type: Number, required: true },
+    cholesterol: { type: Number, required: true },
+    sodium: { type: Number, required: true },
+    totalCarbohydrates: { type: Number, required: true },
+    protein: { type: Number, required: true },
+    vitaminD: { type: Number, required: true },
+    calcium: { type: Number, required: true },
+    iron: { type: Number, required: true },
+    potassium: { type: Number, required: true },
+    vitaminA: { type: Number, required: true },
+    vitaminC: { type: Number, required: true },
+
+    // Jejak Karbon
+    karbonMakanan: { type: Number, required: true },
+    karbonPengolahan: { type: Number, required: true },
+    karbonTransportasiLimbah: { type: Number, required: true },
 });
 
-const Product = mongoose.models.product || mongoose.model('product', productSchema);
-
-export default Product;
+// Gunakan nama model 'Product' (dengan huruf besar) secara konsisten
+export default mongoose.models.Product || mongoose.model("Product", ProductSchema);
